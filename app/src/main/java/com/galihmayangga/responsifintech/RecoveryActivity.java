40b5fba68137ec3a37bc13ab6ef9475414f90a07
galihmay <galihmaya1@gmail.com>
2021-07-06 19:25:37 +0700

    UI Recovery

--- app/src/main/java/com/galihmayangga/responsifintech/RecoveryActivity.java
+++ app/src/main/java/com/galihmayangga/responsifintech/RecoveryActivity.java
@@ -1,14 +1,23 @@
 package com.galihmayangga.responsifintech;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RecoveryActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Recovery Account");
         setContentView(R.layout.activity_recovery);
+    }
+
+    public void Login(View view) {
+        Intent intent = new Intent(RecoveryActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
