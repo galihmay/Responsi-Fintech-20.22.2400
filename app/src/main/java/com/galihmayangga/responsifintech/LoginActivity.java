68255b2d45487d61eee9a9f758c77d0622d6b0a0
galihmay <galihmaya1@gmail.com>
2021-07-06 20:25:53 +0700

    UI Register dan Rev1 UI Login

--- app/src/main/java/com/galihmayangga/responsifintech/LoginActivity.java
+++ app/src/main/java/com/galihmayangga/responsifintech/LoginActivity.java
@@ -5,13 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+   Button buttonLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
         setContentView(R.layout.activity_login);
+
+        buttonLogin = (Button) findViewById(R.id.btnLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }});
     }
 
     public void Recovery(View view) {
@@ -23,4 +37,6 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
         startActivity(intent);
     }
+
+
 }
--- app/src/main/java/com/galihmayangga/responsifintech/RegisterActivity.java
+++ app/src/main/java/com/galihmayangga/responsifintech/RegisterActivity.java
@@ -2,13 +2,26 @@ package com.galihmayangga.responsifintech;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
+    Button buttonRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Register Account");
         setContentView(R.layout.activity_register);
+
+        buttonRegister = (Button) findViewById(R.id.btnRegister);
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(RegisterActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }});
     }
 }
