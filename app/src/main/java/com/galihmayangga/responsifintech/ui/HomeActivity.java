470865f9b08518ab14f5a00ebf062a0e73aa9203
galihmay <galihmaya1@gmail.com>
2021-07-10 17:59:27 +0700

    Profile Picture,rev1 home,dan perbaiki scrollview

--- app/src/main/java/com/galihmayangga/responsifintech/ui/HomeActivity.java
+++ app/src/main/java/com/galihmayangga/responsifintech/ui/HomeActivity.java
@@ -3,6 +3,7 @@ package com.galihmayangga.responsifintech.ui;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -25,10 +26,12 @@ public class HomeActivity extends AppCompatActivity implements HomeMvvm.View {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("APK Bayar Dulu");
         ActivityHomeBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_home);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.profilepicture);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
         binding.setVm(new HomeViewModel(this));
-
-
     }
 
     @Override
