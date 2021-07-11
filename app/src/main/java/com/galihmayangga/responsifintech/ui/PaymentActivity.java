cfa891535b31119ace641448ff3a532957f98e20
galihmay <galihmaya1@gmail.com>
2021-07-11 14:45:21 +0700

    UI Payment

--- app/src/main/java/com/galihmayangga/responsifintech/ui/PaymentActivity.java
+++ app/src/main/java/com/galihmayangga/responsifintech/ui/PaymentActivity.java
@@ -2,7 +2,9 @@ package com.galihmayangga.responsifintech.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.galihmayangga.responsifintech.R;
 
@@ -11,6 +13,12 @@ public class PaymentActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Payment");
         setContentView(R.layout.activity_payment);
+    }
+
+    public void Home(View view) {
+        Intent intent = new Intent(PaymentActivity.this, HomeActivity.class);
+        startActivity(intent);
     }
 }
