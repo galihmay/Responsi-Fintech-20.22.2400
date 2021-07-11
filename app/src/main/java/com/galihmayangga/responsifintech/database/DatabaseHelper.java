75a8c477630c249d0d5063076df8b21d0f1342b1
galihmay <galihmaya1@gmail.com>
2021-07-11 22:37:23 +0700

    Database Helper SQLite dan database Register

---
+++ app/src/main/java/com/galihmayangga/responsifintech/database/DatabaseHelper.java
@@ -0,0 +1,78 @@
+package com.galihmayangga.responsifintech.database;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public DatabaseHelper(Context context) {
+        super(context, "loginSQLite.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE session(id integer PRIMARY KEY, login text)");
+        db.execSQL("CREATE TABLE user(id integer PRIMARY KEY AUTOINCREMENT, email text, password text, name text, address txt)");
+        db.execSQL("INSERT INTO session(id, login) VALUES(1, 'kosong')");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS session");
+        db.execSQL("DROP TABLE IF EXISTS user");
+        onCreate(db);
+    }
+
+    //cek
+    public Boolean checkSession(String sessionValues) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM session WHERE login = ?", new String[]{sessionValues});
+        if (cursor.getCount() > 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+
+    public Boolean upgradeSession(String sessionValues, int id) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("login", sessionValues);
+        long update = db.update("session", contentValues, "id="+id, null);
+        if (update == -1) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    public Boolean insertUser(String email, String password) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("email", email);
+        contentValues.put("password", password);
+        long insert = db.insert("user", null, contentValues);
+        if (insert == -1) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+    //check login
+    public Boolean checkLogin(String email, String password) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM user WHERE email = ? AND password = ?", new String[]{email, password});
+        if (cursor.getCount() > 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
--- app/src/main/java/com/galihmayangga/responsifintech/ui/RegisterActivity.java
+++ app/src/main/java/com/galihmayangga/responsifintech/ui/RegisterActivity.java
@@ -6,11 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.galihmayangga.responsifintech.R;
+import com.galihmayangga.responsifintech.database.DatabaseHelper;
 
 public class RegisterActivity extends AppCompatActivity {
+    DatabaseHelper db;
     Button buttonRegister;
+    EditText EditTextEmail, EditTextPassword, EditTextConfirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +23,37 @@ public class RegisterActivity extends AppCompatActivity {
         setTitle("Register Account");
         setContentView(R.layout.activity_register);
 
+        db = new DatabaseHelper(this);
+
+        EditTextEmail = (EditText)findViewById(R.id.etEmailReg);
+        EditTextPassword = (EditText)findViewById(R.id.etPasswordReg);
+        EditTextConfirmPassword = (EditText)findViewById(R.id.etConfirmPasswordReg);
         buttonRegister = (Button) findViewById(R.id.btnRegister);
+
+
+
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(RegisterActivity.this, HomeActivity.class);
-                startActivity(intent);
-            }});
+            public void onClick(View v) {
+                String strEmail = EditTextEmail.getText().toString();
+                String strPassword = EditTextPassword.getText().toString();
+                String strPasswordConf = EditTextConfirmPassword.getText().toString();
+                if (strPassword.equals(strPasswordConf)) {
+                    Boolean daftar = db.insertUser(strEmail, strPassword);
+                    if (daftar == true) {
+                        Toast.makeText(getApplicationContext(), "Daftar Berhasil", Toast.LENGTH_SHORT).show();
+                        Intent Intent = new Intent(RegisterActivity.this, HomeActivity.class);
+                        startActivity(Intent);
+                        finish();
+                    }
+                    else {
+                        Toast.makeText(getApplicationContext(), "Daftar Gagal", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Password Salah", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });;
     }
 }
