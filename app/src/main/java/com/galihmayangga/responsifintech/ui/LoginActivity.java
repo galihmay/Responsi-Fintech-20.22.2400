8a2018e9080b01ca5f0d438932b1c8dacd3cccf1
galihmay <galihmaya1@gmail.com>
2021-07-11 23:05:10 +0700

    Database Login

--- app/src/main/java/com/galihmayangga/responsifintech/ui/LoginActivity.java
+++ app/src/main/java/com/galihmayangga/responsifintech/ui/LoginActivity.java
@@ -7,11 +7,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.galihmayangga.responsifintech.R;
+import com.galihmayangga.responsifintech.database.DatabaseHelper;
 
 public class LoginActivity extends AppCompatActivity {
+    DatabaseHelper db;
    Button buttonLogin;
+   EditText EditTextEmail, EditTextPassword;
 
 
     @Override
@@ -21,13 +26,32 @@ public class LoginActivity extends AppCompatActivity {
         getSupportActionBar().hide(); // hide the title bar
         setContentView(R.layout.activity_login);
 
+        db = new DatabaseHelper(this);
+
+        EditTextEmail = (EditText)findViewById(R.id.etEmailLog);
+        EditTextPassword = (EditText)findViewById(R.id.etPasswordLog);
         buttonLogin = (Button) findViewById(R.id.btnLogin);
+
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(LoginActivity.this, HomeActivity.class);
-                startActivity(intent);
-            }});
+            public void onClick(View v) {
+                String strUsername = EditTextEmail.getText().toString();
+                String strPassword = EditTextPassword.getText().toString();
+                Boolean masuk = db.checkLogin(strUsername, strPassword);
+                if (masuk == true) {
+                    Boolean updateSession = db.upgradeSession("ada", 1);
+                    if (updateSession == true) {
+                        Toast.makeText(getApplicationContext(), "Login Berhasil", Toast.LENGTH_SHORT).show();
+                        Intent Intent = new Intent(LoginActivity.this, HomeActivity.class);
+                        startActivity(Intent);
+                        finish();
+                    }
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Login Gagal, Periksa Email & Password", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     public void Recovery(View view) {
