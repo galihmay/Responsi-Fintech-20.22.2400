466b82f76a9c149624bb948315226e158723df1b
galihmay <galihmaya1@gmail.com>
2021-07-11 14:33:02 +0700

    isi fragment payment dan ubah warna background fragment

--- app/src/main/java/com/galihmayangga/responsifintech/ui/HistoryFragment.java
+++ app/src/main/java/com/galihmayangga/responsifintech/ui/HistoryFragment.java
@@ -1,17 +1,26 @@
 package com.galihmayangga.responsifintech.ui;
 
+import android.media.ToneGenerator;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.galihmayangga.responsifintech.R;
 
 
+
 public class HistoryFragment extends Fragment {
+
+
+
+
+
 
     public HistoryFragment() {
 
--- app/src/main/java/com/galihmayangga/responsifintech/ui/PaymentFragment.java
+++ app/src/main/java/com/galihmayangga/responsifintech/ui/PaymentFragment.java
@@ -1,17 +1,25 @@
 package com.galihmayangga.responsifintech.ui;
 
+import android.content.Intent;
+import android.graphics.Camera;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.galihmayangga.responsifintech.R;
 
 
 public class PaymentFragment extends Fragment {
+
+
 
     public PaymentFragment() {
 
@@ -20,6 +28,16 @@ public class PaymentFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_payment, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_payment, container, false);
+        TextView txtpaymentcode = view.findViewById(R.id.txtpaymentcode);
+        txtpaymentcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), PaymentActivity.class));
+            }
+        });
+
+    return  view;
     }
 }
